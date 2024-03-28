2f075e6077697eea2041404b2b0d502f893b9dd6
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-03-28 19:43:00 +0530

    Added globe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/custom/HeroSection";
 import LampSection from "./components/custom/LampSection";
 import IpadSection from "./components/custom/IpadSection";
 import HeroParallaxSection from "./components/custom/HeroParallax";
+import GlobeSection from "./components/custom/GlobeSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <IpadSection />
       <HeroParallaxSection />
+      <GlobeSection />
       <LampSection />
     </main>
   );
